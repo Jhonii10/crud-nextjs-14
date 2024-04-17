e370d76edd57c2c7a2d215d23a9532118bd4175c
Jhonni10 <jhonieripianoscue@gmail.com>
2024-04-17 16:57:24 -0500

    config the prisma and init of create api taks

--- app/page.js
+++ app/page.js
@@ -1,9 +1,8 @@
 
 export default function Home() {
   return (
-    <main className='h-screen bg bg-black text-cyan-50 flex justify-center items-center'>
+    <main className='h-screen flex justify-center items-center'>
       <p>Inicio</p>
     </main>
   );
 }
-  
