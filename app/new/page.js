a2e687c17ab944c6280a3cdd21319efea9ef88ad
Jhonni10 <jhonieripianoscue@gmail.com>
2024-04-18 12:15:52 -0500

    create new taks in database

--- app/new/page.js
+++ app/new/page.js
@@ -1,9 +1,12 @@
+
 import React from 'react'
+import FormTaks from '../components/form-taks';
+
+export const metadata = {
+    title: "create task",
+    description: "create new task in database",
+  };
 
 export default function NewPage() {
-  return (
-    <div>
-      NewPage
-    </div>
-  )
+    return <FormTaks />
 }
